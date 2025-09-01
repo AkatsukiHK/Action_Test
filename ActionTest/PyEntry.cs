6176d7872f41161424e2f22c5cdb5e7ea9957479
AkatsukiHK <wirkwitz@gmail.com>
2025-09-01 15:15:28 +0800

    Try Python.

---
+++ ActionTest/PyEntry.cs
@@ -0,0 +1,29 @@
+using System;
+using Python.Runtime;
+namespace ActionTest
+{
+    public class PyEntry
+    {
+        public static void Run()
+        {
+            PythonEngine.Initialize();
+            using (Py.GIL())
+            {
+                try
+                {
+                    string scriptPath = "./Scripts";
+                    dynamic sys = Py.Import("sys");
+                    sys.path.append(scriptPath);
+                    dynamic process = Py.Import("mymodule");
+                    process.greet("World");
+                }
+                catch (PythonException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+            PythonEngine.Shutdown();
+        }
+    }
+}
